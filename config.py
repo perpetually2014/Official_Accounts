33964b7e9713b2e4d56592cbecb038b432cee351
Perpetually <perpetually2014@icloud.com>
2023-06-07 10:48:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import random
 # mysql 服务器
 host = "127.0.0.1"
@@ -14,27 +13,20 @@ redis_port = 6379
 redis_password = '123'
 wechat_md5 = 'news'
 
-
-
 # 公众号fakeid,抓包获取
-fakeids = {'MzAwNDA3Nzg5MA==': '杭州统一战线', 'MzAwODQ4OTY3NQ==': '杭州知联', 'MzAxMjA0NDk2NQ==': '滨江统一战线',
-           'MzAxMjA5NDE5MA==': '西湖统一战线', 'MzA4NjU1OTE4Mg==': '拱墅统一战线', 'MjM5MzM2MDk3OA==': '杭州市上城区统一战线',
-           'MzA3MjA4MjQ5MQ==': '余杭统一战线', 'MzAwNjEwMDI0Mg==': '萧山统一战线', 'MzA5MDQ0NjQxNg==': '富阳统一战线',
-           'MzU0Mjc2MTM4NQ==': '建德统一战线', 'MjM5OTYwNzQ3Nw==': '淳安统一战线', 'MjM5NjQwNzgxMQ==': '临安统站统一战线'
-           }
+fakeids = {'MTI0MDU3NDYwMQ==': '央视新闻', 'MjM5ODEyOTAyMA==': '今日头条'}
 
 # Es配置
-es_index = 'news_m2'
-es_hosts = ['192.168.20.164:9200', '192.168.20.165:9200', '192.168.20.166:9200']
+es_index = 'data'
+es_hosts = ['127.0.0.1:9200', '127.0.0.2:9200', '127.0.0.3:9200']
 
 # kafka配置
-TOPIC = 'news_v1'
-TOPIC2 = 'news_images'
-SERVER = '192.168.20.211:9092, 192.168.20.212:9092, 192.168.20.213:9092'
+TOPIC = 'data1'
+SERVER = '127.0.0.1:9092, 127.0.0.2:9092, 127.0.0.3:9092'
 
 # 微信账号
-Wechat_list = ['内部优惠折扣券', '溯源防伪开放平台', '超级玛2 Pro', '超级码3 Test']
-Wechat_account = random.choice(['内部优惠折扣券', '溯源防伪开放平台', '超级玛2 Pro', '超级码3 Test'])
+Wechat_list = ['data1', 'data2', 'data3']
+Wechat_account = random.choice(['data1', 'data2', 'data3'])
 
 # dingding接受cookie电话
 wechat = 'wechat'
